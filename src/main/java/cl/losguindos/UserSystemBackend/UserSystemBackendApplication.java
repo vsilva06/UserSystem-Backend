9bd95674d3127823766ff0cbb006d1b37313816b
vsilva06 <v.silva06@ufromail.cl>
2023-06-21 22:50:45 -0400

    Init repo

---
+++ src/main/java/cl/losguindos/UserSystemBackend/UserSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.losguindos.UserSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/losguindos/UserSystemBackend/UserSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.losguindos.UserSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
