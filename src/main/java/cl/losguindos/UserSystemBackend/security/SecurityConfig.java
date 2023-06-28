18385abd002256852da8092d8bad438fdeae23d2
vsilva06 <v.silva06@ufromail.cl>
2023-06-27 21:29:28 -0400

    update cors

--- src/main/java/cl/losguindos/UserSystemBackend/security/SecurityConfig.java
+++ src/main/java/cl/losguindos/UserSystemBackend/security/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**" +
                         "")
-                .allowedOrigins("http://localhost:5173") // Replace with your React frontend URL
+                .allowedOrigins("http://localhost:8000") // Replace with your React frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
